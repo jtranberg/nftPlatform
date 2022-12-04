07831d2199c9427316b62ba7c51a040775f334ac
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-12-04 15:09:53 -0800

    last commit before frunt end, trullfe tested,compiled and migrate successful!!!

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -2,75 +2,71 @@
 
 pragma solidity ^0.8.0;
 
+import './ERC165.sol';
+import './interfaces/IERC721.sol';
 
-contract ERC721 {
+contract ERC721 is ERC165, IERC721{
 
-    event Transfer(
-        address indexed from, 
-        address indexed to, 
-        uint256 indexed  tokenId);
-
-        event Approval(
-            address indexed owner,
-            address indexed approved,
-            uint256 indexed tokenId
-        );
-     
     mapping(uint256 => address ) private _tokenOwner;
     mapping(address => uint256) private _OwnedTokensCount;
     mapping(uint256 => address)  private _tokenApprovals;
     
+    constructor() {
+       _registerInterface(bytes4(keccak256('balanceOf(bytes4)')^
+        keccak256('ownerOf(bytes4)')^
+          keccak256('transferFrom(bytes4)')));
+    }
 
-    function balanceOf(address _owner) public view returns(uint256) {
+    function balanceOf(address _owner) public view override returns(uint256) {
       require(_owner != address(0), 'token non exsits');
       return _OwnedTokensCount[_owner];
     }
 
-    function ownerOf(uint256 _tokenId) public view returns (address) {
-        address owner = _tokenOwner[_tokenId];
-        require(owner != address(0), 'owner query for non exsistant nft');
-        return owner;
+    function ownerOf(uint256 _tokenId) public view override returns (address) {
+      address owner = _tokenOwner[_tokenId];
+      require(owner != address(0), 'owner query for non exsistant nft');
+      return owner;
     }
     function _exists(uint256 tokenId) internal view returns(bool) {
-        address owner = _tokenOwner[tokenId];
-        return owner != address(0);
+      address owner = _tokenOwner[tokenId];
+      return owner != address(0);
     }
   
     function _mint( address to, uint256 tokenId) internal virtual{
-        require(to != address(0), "ERC721 : minting to zero address");
-        require(!_exists(tokenId), "ERC721 token already exists");
-          _tokenOwner[tokenId] = to;
-          _OwnedTokensCount[to] += 1;
+      require(to != address(0), "ERC721 : minting to zero address");
+      require(!_exists(tokenId), "ERC721 token already exists");
+        _tokenOwner[tokenId] = to;
+        _OwnedTokensCount[to] += 1;
 
           emit Transfer(address(0), to, tokenId);
        }
     
 
     function _transferFrom(address _from, address _to, uint256 _tokenId) internal {
-        require (_to != address(0), 'ERC721 Transfer to zero address');
-        require(ownerOf(_tokenId) == _from, "does not own address ");
+      require (_to != address(0), 'ERC721 Transfer to zero address');
+      require(ownerOf(_tokenId) == _from, "does not own address ");
         _OwnedTokensCount[_from] -= 1;
         _OwnedTokensCount[_to] += 1;
         _tokenOwner[_tokenId] = _to;
-        emit Transfer(_from, _to, _tokenId);
+          emit Transfer(_from, _to, _tokenId);
         }
 
-        function transferFrom(address _from, address _to, uint256 _tokenId)public {
-           require(isApprovedOrOwner(msg.sender, _tokenId));
-           _transferFrom(_from, _to, _tokenId);
+    function transferFrom(address _from, address _to, uint256 _tokenId) override public {
+      require(isApprovedOrOwner(msg.sender, _tokenId));
+        _transferFrom(_from, _to, _tokenId);
         }
 
-        function approve(address _to, uint tokenId) public {
-            address owner = ownerOf(tokenId);
-            require(_to != owner, 'Error - approval to current owner');
-            require(msg.sender == owner, 'current caller is nt token owner');
-            _tokenApprovals[tokenId] = _to;
+    function approve(address _to, uint tokenId) public {
+      address owner = ownerOf(tokenId);
+        require(_to != owner, 'Error - approval to current owner');
+        require(msg.sender == owner, 'current caller is nt token owner');
+        _tokenApprovals[tokenId] = _to;
             emit Approval(owner, _to, tokenId);
 
         }
      
-       function isApprovedOrOwner(address spender, uint256 tokenId) internal view returns(bool) {
-          require(_exists(tokenId), 'token does not exist');
+    function isApprovedOrOwner(address spender, uint256 tokenId) internal view returns(bool) {
+        require(_exists(tokenId), 'token does not exist');
           address owner = ownerOf(tokenId);
           return(spender == owner);
         
--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -3,8 +3,9 @@
 pragma solidity ^0.8.0;
 
 import './ERC721.sol';
+import './interfaces/IERC721Enumerable.sol';
 
-contract ERC721Enumerable is ERC721 {
+contract ERC721Enumerable is IERC721Enumerable, ERC721 {
     
     uint256[] private _allTokens;
 
@@ -12,6 +13,11 @@ contract ERC721Enumerable is ERC721 {
     mapping(address => uint256[]) private _ownedTokens; //list of owner tokens
     mapping(uint => uint) private _ownedTokensIndex;   //tokenId to index
 
+        constructor() {
+       _registerInterface(bytes4(keccak256('toalSupply(bytes4)')^
+        keccak256('tokenByIndex(bytes4)')^
+          keccak256('tokenOfOwnerByIndex(bytes4)')));
+        }
     function _mint(address to, uint256 tokenId) internal override(ERC721){
        super._mint(to, tokenId);
       _addTokensToAllTokenEnumeration(tokenId);
@@ -29,18 +35,18 @@ contract ERC721Enumerable is ERC721 {
          
     }
 
-    function tokenByIndex(uint256 index ) public view returns (uint256) {
+    function tokenByIndex(uint256 index ) public view override returns (uint256) {
         require (index < totalSupply(), "global index out of bounds"); 
         return _allTokens[index];
     }
 
-    function tokenOfOwnerByIndex(address owner, uint index) public view returns(uint256) {
+    function tokenOfOwnerByIndex(address owner, uint index) public view override returns(uint256) {
         require(index < balanceOf(owner),"owner index out of bounds");
         return _ownedTokens[owner] [index];
     }
      
 //return the total supply of the _allTokens array
-    function totalSupply() public view returns(uint256) {
+    function totalSupply() public view override returns(uint256) {
         return _allTokens.length;
     }
 }
