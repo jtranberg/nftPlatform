53476de2d4eea1490a99b40fd67a0b312d38b125
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-12-04 17:20:38 -0800

    commit after first .js test

---
+++ test/KryptoBird.test.js
@@ -0,0 +1,25 @@
+const {assert} = require ("chai");
+
+
+
+const KryptoBird = artifacts.require('./Kryptobird');
+//check for chai
+require("chai")
+.use(require("chai-as-promised"))
+.should()
+
+contract('KryptoBird', (accounts) => {
+    let contract 
+
+    describe('deployment', async() => {
+
+       it('deploys successfuly', async() => {
+        contract =  await KryptoBird.deployed()
+        const address = contract.address;
+        assert.notEqual(address,'');
+        assert.notEqual(address,null);
+        assert.notEqual(address,undefined);
+        assert.notEqual(address, 0x0);
+       })
+    })
+})
